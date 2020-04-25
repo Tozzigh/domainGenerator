7f8bf96203036b62e5659809378f19fe1c9cdf8a
Tozzigh <tozzigh@gmail.com>
2020-04-25 08:53:16 +0000

    Ex Domain

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function createDomain(){
+    var elementi =
+    {
+    who : ["grandma","alien","sister","ghost","jester"],
+    thing : ["banana","flower","baguette","cannolo","knife","pizza"],
+    where : ["eye","face","feet","mouth","nose"],
+    }
+}
