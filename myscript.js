3f426806062370d638ec2a8b6e9f5aaa05056d28
Tozzigh <tozzigh@gmail.com>
2020-04-25 13:46:29 +0000

    All arrays must have same length

--- myscript.js
+++ myscript.js
@@ -1,8 +1,22 @@
-function createDomain(){
+function changeText() {
     var elementi =
     {
-    who : ["grandma","alien","sister","ghost","jester"],
-    thing : ["banana","flower","baguette","cannolo","knife","pizza"],
-    where : ["eye","face","feet","mouth","nose"],
+        who: ["grandma", "alien"],
+        thing: ["banana", "flower"],
+        where: ["eye", "face"],
+    }
+
+    var arrayDom = []
+    
+    for (let x = 0; x < (elementi.who).length; x++) {
+        for (let y = 0; y < (elementi.who).length; y++) {
+            for (let z = 0; z < (elementi.who).length; z++) {
+                arrayDom.push(elementi.who[x].concat(elementi.thing[y], elementi.where[z], ".com", "<br>"))
+            }
+        }
+    }
+    for (let dom in arrayDom){
+        document.write("<h1>"+arrayDom[dom]+"</h1>");
     }
 }
+changeText();
